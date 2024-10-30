beaa2eec04f3f05837d868bd5a85b12ff96ede9e
Serena Harris <sharri23@uvm.edu>
2024-10-30 12:41:57 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 const showmeIdeas = () => {
-    // set up variables
-    const heading = document.getElementById('heading');
-    const octoberIsChecked = document.getElementById('sweettreats').checked;
-    const novemberIsChecked = document.getElementById('savorytreats').checked;
+    const sweetList = document.getElementById('sweet-list');
+    const savoryList = document.getElementById('savory-list');
+    const sweetIsChecked = document.getElementById('sweettreats').checked;
+    const savoryIsChecked = document.getElementById('savorytreats').checked;
+
+    // Hide lists
+    sweetList.style.display = 'none';
+    savoryList.style.display = 'none';
+
+    // Show the selected list
+    if (sweetIsChecked) {
+        sweetList.style.display = 'block';
+    } else if (savoryIsChecked) {
+        savoryList.style.display = 'block';
+    }
+};
