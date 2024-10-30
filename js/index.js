ddb0911c3ca67d07d87a0dddccc16050649919dd
Serena <sharri23@uvm.edu>
2024-10-30 12:08:17 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const showmeIdeas = () => {
+    // set up variables
+    const heading = document.getElementById('heading');
+    const octoberIsChecked = document.getElementById('sweettreats').checked;
+    const novemberIsChecked = document.getElementById('savorytreats').checked;
